d0140cdd6d8cefb711ab9b87420bf28c63fd168e
SirMatters <perov.krll@gmail.com>
2020-06-12 18:15:22 +0300

    <AddEntry/>
    - add increment(), decrement(), slide() methods to handle values change
    
    - add <DateHeader/>

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,77 @@
 import React from 'react';
 import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlider from './UdaciSlider';
+import UdaciStepper from './UdaciStepper';
+import DateHeader from './DateHeader';
 
 export default class AddEntry extends React.Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+    this.setState((prevState) => {
+      const count = prevState[metric] + step;
+      return { ...prevState, [metric]: count > max ? max : count };
+    });
+  };
+
+  decrement = (metric) => {
+    this.setState((prevState) => {
+      const count = prevState[metric] - getMetricMetaInfo(metric).step;
+      return { ...prevState, [metric]: count < 0 ? 0 : count };
+    });
+  };
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value,
+    }));
+  };
+
   render() {
-    return <View>{getMetricMetaInfo('bike').getIcon()}</View>;
+    const metaInfo = getMetricMetaInfo();
+    const date = new Date().toLocaleDateString();
+    return (
+      <View>
+        <DateHeader date={date} />
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider' ? (
+                <UdaciSlider
+                  value={this.state[key]}
+                  onChange={() => {
+                    this.slide(key, value);
+                  }}
+                  {...rest}
+                />
+              ) : (
+                <UdaciStepper
+                  value={value}
+                  onIncrement={() => {
+                    this.increment(key);
+                  }}
+                  onDecrement={() => {
+                    this.decrement(key);
+                  }}
+                  {...rest}
+                />
+              )}
+            </View>
+          );
+        })}
+      </View>
+    );
   }
 }
