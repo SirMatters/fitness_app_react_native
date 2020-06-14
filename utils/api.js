f608193c5791128f5ff54bad19400d4a5a93b583
SirMatters <perov.krll@gmail.com>
2020-06-14 22:10:48 +0300

    Sample calendar data - create <History/> component
    /utils/api.js
    - add fetchCalendarResults() function to get data from calendar (asyncstorage)
    
    Known bugs:
    - null values are populated to async storage

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import { CALENDAR_STORAGE_KEY } from './_calendar';
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from './_calendar';
 
 export const submitEntry = async ({ entry, key }) => {
   return await AsyncStorage.setItem(
@@ -13,4 +13,10 @@ export const removeEntry = async (key) => {
   data[key] = undefined;
   delete data[key];
   return await AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+};
+
+export const fetchCalendarResults = async () => {
+  return formatCalendarResults(
+    await AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+  );
 };
