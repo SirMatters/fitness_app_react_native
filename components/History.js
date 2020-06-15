bd9adf23022adc6b8d7bb5bf63a7119688570851
SirMatters <perov.krll@gmail.com>
2020-06-15 18:00:01 +0300

    History display and styling <History/> - chagne renderItem() and renderEmptyDate() methods to display proper information and components - create and apply styles
    <MetricCard/>
    - create component
    - import component to <History /> to be displayed
    - create and apply component styles

--- components/History.js
+++ components/History.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalendarResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
+import MetricCard from './MetricCard';
+import DateHeader from './DateHeader';
+import { white } from '../utils/colors';
 
 class History extends Component {
   componentDidMount() {
@@ -23,18 +32,26 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today ? (
-        <Text>{JSON.stringify(today)}</Text>
+        <View>
+          <DateHeader date={formattedDate} />
+          <Text style={styles.noDataText}>{today}</Text>
+        </View>
       ) : (
-        <Text>{JSON.stringify(metrics)}</Text>
+        <TouchableOpacity onPress={() => console.log('Pressed!')}>
+          <MetricCard metrics={metrics} date={formattedDate} />
+        </TouchableOpacity>
       )}
     </View>
   );
 
   renderEmptyDate = (formattedDate) => (
-    <View>
-      <Text>No data for this day</Text>
+    <View style={styles.item}>
+      <DateHeader data={formattedDate} />
+      <Text style={styles.noDataText}>
+        You did not log any data for this day
+      </Text>
     </View>
   );
 
@@ -49,6 +66,30 @@ class History extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
 
 const mapStateToProps = (entries) => ({ entries });
 
