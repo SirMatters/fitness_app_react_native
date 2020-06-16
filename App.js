aec77c7650e2bf60f2aa6ce6b9b7e7dbb20d471d
SirMatters <perov.krll@gmail.com>
2020-06-16 18:09:36 +0300

    Tabs - add tabs to <App/> component - current configuration compiles

--- App.js
+++ App.js
@@ -5,14 +5,23 @@ import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default class App extends React.Component {
   render() {
+    const Tab = createBottomTabNavigator();
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           <View style={{ height: 20 }} />
-          <History />
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen name='History' component={History} />
+              <Tab.Screen name='Add New' component={AddEntry} />
+            </Tab.Navigator>
+          </NavigationContainer>
+          {/* <History /> */}
         </View>
       </Provider>
     );
